01f47f3e7d4ea3a27c18402193a1b4065089a095
Nicholas-Ho <j.xiong@live.com>
2023-08-11 15:46:34 +0100

    Minor edits to aggs getter and added ticker getter

--- data/scripts/polygon_io/polygon_io_fetcher.py
+++ data/scripts/polygon_io/polygon_io_fetcher.py
@@ -2,6 +2,7 @@
 
 from polygon import RESTClient
 from polygon.rest.models.aggs import Agg
+from polygon.rest.models.tickers import Ticker
 import json
 from enum import Enum
 from datetime import date, timedelta
@@ -11,7 +12,8 @@ import os
 
 # Constants
 API_KEYS_FILE = ".secrets"
-MAX_POLYGON_LIMIT = 50000 # as defined by Polygon.io's API
+MAX_POLYGON_AGGS_LIMIT = 50000 # as defined by Polygon.io's API
+MAX_POLYGON_TICKER_LIMIT = 1000 # as defined by Polygon.io's API
 
 # Duration enum for the timespan argument
 class Duration(Enum):
@@ -35,7 +37,9 @@ class PolygonIOFetcher:
             json_content = json.load(f)
             return json_content[service]["api_key"]
         
-    def getAggregateData(self, ticker: str, interval: int, interval_timespan: Duration, date_from: date, date_to: date, limit: int):
+    # Aggregates
+
+    def getAggregateData(self, ticker: str, interval: int, interval_timespan: Duration, date_from: date, date_to: date, adjusted: bool, limit: int) -> List[Agg]:
         '''
         ticker: Target ticker
 
@@ -47,11 +51,13 @@ class PolygonIOFetcher:
 
         date_to: End date of the data fetched
 
+        adjusted: Whether the data is adjusted for splits
+
         limit: Limit to the number of data points fetched. Polygon.io defines the maximum limit to be 50000
         '''
         output = []
 
-        # Pseudo-pagination by splitting the date range into multiple ranges of length self.max_date_range
+        # Pagination by splitting the date range into multiple ranges of length self.max_date_range
         curr_date_to = date_to
         while curr_date_to - date_from >= self.max_date_range:
             curr_date_from = curr_date_to - self.max_date_range
@@ -63,10 +69,13 @@ class PolygonIOFetcher:
                 timespan=interval_timespan.value,
                 from_=curr_date_from,
                 to=curr_date_to,
+                adjusted=adjusted,
                 limit=limit
             )
             if len(aggs) >= limit:
                 raise Exception("Polygon.io retrieval limit reached")
+            if len(aggs) <= 0:
+                return output
             output += aggs
 
             curr_date_to = curr_date_from - timedelta(days=1)
@@ -82,11 +91,13 @@ class PolygonIOFetcher:
         )
         if len(aggs) >= limit:
             raise Exception("Polygon.io retrieval limit reached")
+        if len(aggs) <= 0:
+            return output
         output += aggs
 
         return output
     
-    def writeAggsToFile(self, filename: str, ticker: str, aggs: List[Agg], append: bool = True) -> None:
+    def writeAggsToCSV(self, filename: str, ticker: str, aggs: List[Agg], append: bool = True) -> None:
         # Add headers if the file has not been created or the file is being overwritten
         header = ("ticker", "open", "high", "close", "volume", "vwap", "timestamp", "transactions", "otc")
         if append and os.path.isfile(filename):
@@ -104,7 +115,7 @@ class PolygonIOFetcher:
                 writer_obj.writerow(header)
 
             for agg in aggs:
-                agg_tuple = (
+                tickers_tuple = (
                     ticker,
                     agg.open,
                     agg.high,
@@ -116,11 +127,154 @@ class PolygonIOFetcher:
                     agg.transactions,
                     agg.otc
                 )
-                writer_obj.writerow(agg_tuple)
+                writer_obj.writerow(tickers_tuple)
+
+    def getWriteAggData(self, ticker: str, interval: int, interval_timespan: Duration, date_from: date, date_to: date, adjusted: bool, limit: int, filename: str) -> None:
+        '''
+        Gets aggregate data and writes it directly to file without returning the data. Avoids loading large amounts of data into memory. 
+
+
+        ticker: Target ticker
+
+        interval: The granularity of the data. Multiply by interval_timespan (eg. interval=5, interval_timespan="minutes")
+
+        interval_timespan: The granularity of the data. Valid values as written in the Duration enum
+
+        date_from: Start date of the data fetched
+
+        date_to: End date of the data fetched
+
+        adjusted: Whether the data is adjusted for splits
+
+        limit: Limit to the number of data points fetched. Polygon.io defines the maximum limit to be 50000
+
+        filename: File to write the data to
+        '''
+
+        # Pagination by splitting the date range into multiple ranges of length self.max_date_range
+        curr_date_to = date_to
+        while curr_date_to - date_from >= self.max_date_range:
+            curr_date_from = curr_date_to - self.max_date_range
+            
+            # Append date ranges, working backwards from date_to in increments of self.max_date_range
+            aggs = self.client.get_aggs(
+                ticker=ticker, 
+                multiplier=interval,
+                timespan=interval_timespan.value,
+                from_=curr_date_from,
+                to=curr_date_to,
+                adjusted=adjusted,
+                limit=limit
+            )
+            if len(aggs) >= limit:
+                raise Exception("Polygon.io retrieval limit reached")
+            if len(aggs) <= 0:
+                return
+            
+            self.writeAggsToCSV(filename, ticker, aggs, True)
+
+            curr_date_to = curr_date_from - timedelta(days=1)
+
+        # Lastly, append any remaining dates
+        aggs = self.client.get_aggs(
+            ticker=ticker, 
+            multiplier=interval,
+            timespan=interval_timespan.value,
+            from_=date_from,
+            to=curr_date_to,
+            limit=limit
+        )
+        if len(aggs) >= limit:
+            raise Exception("Polygon.io retrieval limit reached")
+        if len(aggs) <= 0:
+            return
+        
+        self.writeAggsToCSV(filename, ticker, aggs, True)
+
+    # Tickers
+
+    def getAllTickers(self, ticker_type: str, market: str, limit: int) -> List[Ticker]:
+        '''
+        ticker_type: Type of ticker. Pass None to query all types
+
+        market: Market to search. Pass None to query all markets
+
+        limit: Limit to the number of data points fetched. Polygon.io defines the maximum limit to be 1000
+        '''
+        # Pagination via sort and ticker_gt
+        sorting_field = 'ticker'
+        output: List[Ticker] = []
+        
+        tickers = self.client.list_tickers(type=ticker_type, market=market, sort=sorting_field, limit=limit)
+        output += tickers
+
+        while len(tickers) > 0:
+            last_ticker = output[-1]
+            tickers = self.client.list_tickers(type=ticker_type, market=market, sort=sorting_field, limit=limit, ticker_gt=last_ticker.ticker)
+            output += tickers
+
+        return output
+    
+    
+    def writeTickersToCSV(self, filename: str, tickers: List[Ticker], append: bool = True) -> None:
+        # Add headers if the file has not been created or the file is being overwritten
+        header = (
+            "ticker",
+            "active",
+            "cik",
+            "composite_figi",
+            "currency_name",
+            "currency_symbol",
+            "base_currency_symbol",
+            "base_currency_name",
+            "delisted_utc",
+            "last_updated_utc",
+            "locale",
+            "market",
+            "primary_exchange",
+            "share_class_figi",
+            "type",
+            "source_feed"
+            )
+        if append and os.path.isfile(filename):
+            add_header = False
+        else:
+            add_header = True
+
+        # Set the mode to append or write
+        mode = 'a' if append else 'w'
+        with open(filename, mode, newline='', encoding='utf-8') as f:
+            writer_obj = writer(f, delimiter=',')
+
+            # Add headers if necessary
+            if add_header:
+                writer_obj.writerow(header)
+
+            for ticker in tickers:
+                tickers_tuple = (
+                    ticker.ticker,
+                    ticker.active,
+                    ticker.cik,
+                    ticker.composite_figi,
+                    ticker.currency_name,
+                    ticker.currency_symbol,
+                    ticker.base_currency_symbol,
+                    ticker.base_currency_name,
+                    ticker.delisted_utc,
+                    ticker.last_updated_utc,
+                    ticker.locale,
+                    ticker.market,
+                    ticker.primary_exchange,
+                    ticker.share_class_figi,
+                    ticker.type,
+                    ticker.source_feed
+                    )
+                writer_obj.writerow(tickers_tuple)
             
 # Testing
 if __name__ == "__main__":
     fetcher = PolygonIOFetcher(API_KEYS_FILE)
     ticker = 'GOOGL'
-    aggs = fetcher.getAggregateData(ticker, 1, Duration.MINUTES, date(year=2022, month=7, day=1), date(year=2023, month=7, day=31), MAX_POLYGON_LIMIT)
-    fetcher.writeAggsToFile("googl.csv", ticker, aggs, append=True)
+    fetcher.getWriteAggData(ticker, 1, Duration.MINUTES, date(year=2022, month=7, day=1), date(year=2023, month=7, day=31), False, MAX_POLYGON_AGGS_LIMIT, "aggs.csv")
+    # tickers = fetcher.getAllTickers("CS", "stocks", 10)
+    # fetcher.writeTickersToCSV('tickers.csv', tickers, append=True)
